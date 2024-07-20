d25ccae70de1f3b08824c4dc840de092a10cf4de
JOB KIPTOO <114221274+KIPTOO-JOB@users.noreply.github.com>
2024-07-20 21:25:14 +0000

    inint

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+
+const dodger = document.getElementById('dodger');
+dodger.style.left = '0px';
+
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10);
+
+
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`;
+  }
+}
